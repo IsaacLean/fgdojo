3706acc9b48deac516fb490e80dd1030996ff4ff
Matthew Isaac Lean <matt@mattlean.com>
2016-01-22 04:30:05 -0800

    Add minification on template HTML, add comments to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var concat = require('gulp-concat');
 var eslint = require('gulp-eslint');
+var htmlmin = require('gulp-htmlmin');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var rename = require('gulp-rename');
@@ -12,7 +13,8 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
-var HTML_SRC = './view/**/*.html';
+var TEMPLATE_SRC = './view/templates/*.html';
+var TEMPLATE_DEST = './view/templates-min';
 var SASS_SRC = './view/sass/**/*.scss';
 var SASS_DEST = './static/css';
 var JS_SRC = './view/js/**/*.js';
@@ -20,10 +22,17 @@ var JS_DEST = './static/js';
 var IMG_SRC = './view/img/*';
 var IMG_DEST = './static/img';
 
-// Run setup-styles first then watch them
+// Setup styles, scripts & images once then watch
 gulp.task('default', ['setup-styles', 'setup-scripts', 'setup-images', 'watch']);
 
-// Recompile Sass into CSS & auto-prefix it
+// Minify templates
+gulp.task('setup-templates-min', function() {
+    return gulp.src(TEMPLATE_SRC)
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest(TEMPLATE_DEST));
+});
+
+// Recompile Sass into CSS, auto-prefix it & inject into browser
 gulp.task('setup-styles', function() {
     gulp.src(SASS_SRC)
         .pipe(sass())
@@ -34,6 +43,7 @@ gulp.task('setup-styles', function() {
         .pipe(browserSync.stream());
 });
 
+// Same as 'setup-styles' except minifies
 gulp.task('setup-styles-min', function() {
     gulp.src(SASS_SRC)
         .pipe(sass({outputStyle: 'compressed'}))
@@ -45,6 +55,7 @@ gulp.task('setup-styles-min', function() {
         .pipe(browserSync.stream());
 });
 
+// Run ESLint and concatenate all JS files
 gulp.task('setup-scripts', function() {
     return gulp.src([JS_SRC])
         .pipe(eslint())
@@ -54,6 +65,8 @@ gulp.task('setup-scripts', function() {
         .pipe(gulp.dest(JS_DEST));
 });
 
+
+// Same as 'setup-scripts' except minifies
 gulp.task('setup-scripts-min', function() {
     return gulp.src([JS_SRC])
         .pipe(sourcemaps.init())
@@ -67,6 +80,7 @@ gulp.task('setup-scripts-min', function() {
         .pipe(gulp.dest(JS_DEST));
 });
 
+// Compress JPEG & PNG images
 gulp.task('setup-images', function() {
     return gulp.src(IMG_SRC)
         .pipe(imagemin({
@@ -76,17 +90,18 @@ gulp.task('setup-images', function() {
         .pipe(gulp.dest(IMG_DEST));
 });
 
-// Auto-recompile Sass files on save, then live reload HTML & CSS in browser
+// Watch any changes in templates, Sass, JS & images and update if changes occur
 gulp.task('watch', function() {
     browserSync.init({
         port: 9002,
         proxy: 'localhost:9001'
     });
 
-    gulp.watch(HTML_SRC).on('change', browserSync.reload);
+    gulp.watch(TEMPLATE_SRC).on('change', browserSync.reload);
     gulp.watch(SASS_SRC, ['setup-styles']);
     gulp.watch(JS_SRC, ['setup-scripts']).on('change', browserSync.reload);
     gulp.watch(IMG_SRC, ['setup-images']).on('change', browserSync.reload);
 });
 
+// Create most optimized build
 gulp.task('build',  ['setup-styles-min', 'setup-scripts-min', 'setup-images']);
