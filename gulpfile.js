844947b03ca406ba87358a3b337168370164b90d
Matthew Isaac Lean <matt@mattlean.com>
2016-01-22 04:03:33 -0800

    Add support for CSS, JS & img optimization

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,77 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
 var eslint = require('gulp-eslint');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
-var HTML_LOC = './view/**/*.html';
-var SASS_LOC = './view/sass/**/*.scss';
-var JS_LOC = './view/js/**/*.js';
+var HTML_SRC = './view/**/*.html';
+var SASS_SRC = './view/sass/**/*.scss';
+var SASS_DEST = './static/css';
+var JS_SRC = './view/js/**/*.js';
+var JS_DEST = './static/js';
+var IMG_SRC = './view/img/*';
+var IMG_DEST = './static/img';
 
-// Run styles first then watch them
-gulp.task('default', ['styles', 'lint', 'watch']);
-
-gulp.task('lint', function() {
-    return gulp.src([JS_LOC])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failOnError())
-    .pipe(gulp.dest('./static'));
-});
+// Run setup-styles first then watch them
+gulp.task('default', ['setup-styles', 'setup-scripts', 'setup-images', 'watch']);
 
 // Recompile Sass into CSS & auto-prefix it
-gulp.task('styles', function() {
-    gulp.src(SASS_LOC)
-    .pipe(sass())
-    .pipe(autoprefixer({
-        browser: ['last 2 versions']
-    }))
-    .pipe(gulp.dest('./static'))
-    .pipe(browserSync.stream());
+gulp.task('setup-styles', function() {
+    gulp.src(SASS_SRC)
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browser: ['last 2 versions']
+        }))
+        .pipe(gulp.dest(SASS_DEST))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('setup-styles-min', function() {
+    gulp.src(SASS_SRC)
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer({
+            browser: ['last 2 versions']
+        }))
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest(SASS_DEST))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('setup-scripts', function() {
+    return gulp.src([JS_SRC])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError())
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest(JS_DEST));
+});
+
+gulp.task('setup-scripts-min', function() {
+    return gulp.src([JS_SRC])
+        .pipe(sourcemaps.init())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failOnError())
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(JS_DEST));
+});
+
+gulp.task('setup-images', function() {
+    return gulp.src(IMG_SRC)
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(IMG_DEST));
 });
 
 // Auto-recompile Sass files on save, then live reload HTML & CSS in browser
@@ -39,7 +83,10 @@ gulp.task('watch', function() {
         proxy: 'localhost:9001'
     });
 
-    gulp.watch(HTML_LOC).on('change', browserSync.reload);
-    gulp.watch(SASS_LOC, ['styles']);
-    gulp.watch(JS_LOC, ['lint']).on('change', browserSync.reload);
+    gulp.watch(HTML_SRC).on('change', browserSync.reload);
+    gulp.watch(SASS_SRC, ['setup-styles']);
+    gulp.watch(JS_SRC, ['setup-scripts']).on('change', browserSync.reload);
+    gulp.watch(IMG_SRC, ['setup-images']).on('change', browserSync.reload);
 });
+
+gulp.task('build',  ['setup-styles-min', 'setup-scripts-min', 'setup-images']);
